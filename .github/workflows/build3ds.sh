e65fc9476d5a78e32a56c195b81ac3ab7ffd5473
jtothebell <j.to.the.bell@gmail.com>
2021-03-10 07:53:19 -0700

    first stab at cia build

--- .github/workflows/build3ds.sh
+++ .github/workflows/build3ds.sh
@@ -1,10 +1,19 @@
 source /etc/profile.d/devkit-env.sh
 
+apt-get update && apt-get install p7zip-full -y
+
 cd /build_dir
 
 #todo: build makerom and bannertool to also build cia
+export ctr_ver=16
+curl -LJO https://github.com/jakcron/Project_CTR/releases/download/v0."$ctr_ver"/makerom_0"$ctr_ver"_ctrtool.zip
+7z e makerom_0"$ctr_ver"_ctrtool.zip -o/usr/bin Ubuntu/makerom
+7z e makerom_0"$ctr_ver"_ctrtool.zip -o/usr/bin Ubuntu/ctrtool
+chmod +x /usr/bin/makerom && chmod +x /usr/bin/ctrtool
 
 #Build 3dsx
 make 3ds
 
-#todo: copy artifacts for uploading
+#Build cia
+make cia
+
