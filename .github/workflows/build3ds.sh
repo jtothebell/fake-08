a4b3991c64ae358de7121dafc70e85156f40a470
jtothebell <j.to.the.bell@gmail.com>
2021-03-10 08:06:24 -0700

    Oops, include bannertool not ctrtool

--- .github/workflows/build3ds.sh
+++ .github/workflows/build3ds.sh
@@ -7,9 +7,10 @@ cd /build_dir
 #todo: build makerom and bannertool to also build cia
 export ctr_ver=16
 curl -LJO https://github.com/jakcron/Project_CTR/releases/download/v0."$ctr_ver"/makerom_0"$ctr_ver"_ctrtool.zip
+curl -LJO https://github.com/Steveice10/bannertool/releases/download/1.2.0/bannertool.zip
 7z e makerom_0"$ctr_ver"_ctrtool.zip -o/usr/bin Ubuntu/makerom
-7z e makerom_0"$ctr_ver"_ctrtool.zip -o/usr/bin Ubuntu/ctrtool
-chmod +x /usr/bin/makerom && chmod +x /usr/bin/ctrtool
+7z e bannertool.zip -o/usr/bin linux-x86_64/bannertool
+chmod +x /usr/bin/makerom && chmod +x /usr/bin/bannertool
 
 #Build 3dsx
 make 3ds
