63ab7944910346d30bd180febaf3c0e482b04c7d
jtothebell <j.to.the.bell@gmail.com>
2021-03-10 08:20:17 -0700

    try building banner tool instead of downloading

--- .github/workflows/build3ds.sh
+++ .github/workflows/build3ds.sh
@@ -7,9 +7,14 @@ cd /build_dir
 #todo: build makerom and bannertool to also build cia
 export ctr_ver=16
 curl -LJO https://github.com/jakcron/Project_CTR/releases/download/v0."$ctr_ver"/makerom_0"$ctr_ver"_ctrtool.zip
-curl -LJO https://github.com/Steveice10/bannertool/releases/download/1.2.0/bannertool.zip
 7z e makerom_0"$ctr_ver"_ctrtool.zip -o/usr/bin Ubuntu/makerom
-7z e bannertool.zip -o/usr/bin linux-x86_64/bannertool
+
+#build bannertool
+git clone https://github.com/Steveice10/bannertool.git --recurse-submodules
+cd bannertool
+make
+sudo cp output/linux-x86_64/bannertool /usr/bin/
+
 chmod +x /usr/bin/makerom && chmod +x /usr/bin/bannertool
 
 #Build 3dsx
