7c0064e7bb9d5f8fe9e678dcf592d140ab72d89c
jtothebell <j.to.the.bell@gmail.com>
2022-01-29 16:40:35 -0700

    Miyoo Mini swap menu and screen stretch

--- platform/miyoomini/source/MiyooMiniHost.cpp
+++ platform/miyoomini/source/MiyooMiniHost.cpp
@@ -294,8 +294,8 @@ InputState_t Host::scanInput(){
                     case SDLK_LSHIFT:currKDown |= P8_KEY_O; break;
                     case SDLK_LALT:  currKDown |= P8_KEY_X; break;
                     case SDLK_LCTRL: currKDown |= P8_KEY_O; break;
-                    case SDLK_RCTRL: done = SDL_TRUE; break;
-                    case SDLK_ESCAPE: stretchKeyPressed = true; break;
+                    case SDLK_ESCAPE: done = SDL_TRUE; break;
+                    case SDLK_RCTRL: stretchKeyPressed = true; break;
                     default: break;
                 }
                 break;
