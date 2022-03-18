21a2149535dbba675696d55560da6ba93c354a80
jtothebell <j.to.the.bell@gmail.com>
2022-03-17 18:10:21 -0600

    Download bannertool release instead of clone/build

--- .github/workflows/build3ds.sh
+++ .github/workflows/build3ds.sh
@@ -4,16 +4,14 @@ apt-get update && apt-get install p7zip-full zip libc6-dev g++ -y
 
 cd /build_dir
 
-#todo: build makerom and bannertool to also build cia
+#dl makerom
 export ctr_ver=16
 curl -LJO https://github.com/jakcron/Project_CTR/releases/download/v0."$ctr_ver"/makerom_0"$ctr_ver"_ctrtool.zip
 7z e makerom_0"$ctr_ver"_ctrtool.zip -o/usr/bin Ubuntu/makerom
 
-#build bannertool
-git clone https://github.com/Steveice10/bannertool.git --recurse-submodules
-cd bannertool
-make
-sudo cp output/linux-x86_64/bannertool /usr/bin/
+#dl bannertool
+curl -LJO https://github.com/Steveice10/bannertool/releases/download/1.2.0/bannertool.zip
+7z e bannertool.zip -o/usr/bin linux-x86_64/bannertool
 
 chmod +x /usr/bin/makerom && chmod +x /usr/bin/bannertool
 
