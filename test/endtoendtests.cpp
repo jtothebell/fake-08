d33607764f8ad18d77ec415a90b5dc94e42ae294
Jon Bell <j.to.the.bell@gmail.com>
2022-04-14 17:38:46 -0600

    Add failing tline test

--- test/endtoendtests.cpp
+++ test/endtoendtests.cpp
@@ -567,6 +567,20 @@ TEST_CASE("Loading and running carts") {
 
         vm->CloseCart();
     }
+    SUBCASE("tline test"){
+        vm->LoadCart("tline_test.p8");
+
+        SUBCASE("No error reported"){
+            CHECK(vm->GetBiosError() == "");
+        }
+        SUBCASE("sceen matches screenshot"){
+            vm->UpdateAndDraw();
+
+            CHECK(verifyScreenshot(vm, host, "carts/screenshots/tline_test_f01.png"));
+        }
+
+        vm->CloseCart();
+    }
     
     delete vm;
     delete host;
