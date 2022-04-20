0cf457de07e2df3dc8144c4a894d038478baed28
Jon Bell <j.to.the.bell@gmail.com>
2022-04-19 18:44:29 -0600

    add failing nested long comment/string e2e test

--- test/endtoendtests.cpp
+++ test/endtoendtests.cpp
@@ -581,6 +581,20 @@ TEST_CASE("Loading and running carts") {
 
         vm->CloseCart();
     }
+    SUBCASE("nested long comment/long string test"){
+        vm->LoadCart("nested_comment_test.p8");
+
+        SUBCASE("No error reported"){
+            CHECK(vm->GetBiosError() == "");
+        }
+        SUBCASE("sceen matches screenshot"){
+            vm->UpdateAndDraw();
+
+            CHECK(verifyScreenshot(vm, host, "carts/screenshots/nested_comment_test_f01.png"));
+        }
+
+        vm->CloseCart();
+    }
     
     delete vm;
     delete host;
