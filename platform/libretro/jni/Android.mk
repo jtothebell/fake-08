94a559afc8b41746bb3580631879c0c3c6bff957
Jon Bell <j.to.the.bell@gmail.com>
2022-07-27 06:51:50 -0600

    tweaks to libretro android build

--- platform/libretro/jni/Android.mk
+++ platform/libretro/jni/Android.mk
@@ -1,17 +1,16 @@
 
 LOCAL_PATH := $(call my-dir)
 
-CORE_DIR := $(LOCAL_PATH)/..
+CORE_DIR := $(LOCAL_PATH)/../../..
 
-include $(CORE_DIR)/Makefile.common
+include $(CORE_DIR)/platform/libretro/Makefile.common
 
 COREFLAGS := -DANDROID -D__LIBRETRO__ -Wall -Wno-deprecated -ffunction-sections -std=c++17 -fno-rtti -fexceptions  $(INCFLAGS)
 
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE        := fake_08_libretro
-LOCAL_CPP_EXTENSION := .cpp .c
+LOCAL_MODULE        := retro
 LOCAL_SRC_FILES     := $(SOURCES_CXX)
 LOCAL_C_INCLUDES    := $(MY_INCLUDES)
 LOCAL_CXXFLAGS      := $(COREFLAGS)
--- platform/libretro/jni/Application.mk
+++ platform/libretro/jni/Application.mk
@@ -1,1 +1,2 @@
 APP_ABI := armeabi-v7a arm64-v8a
+APP_PLATFORM := android-16
