7bb5d70ee8a526113800bbf4bce5924a5a0c8b38
Jon Bell <j.to.the.bell@gmail.com>
2022-07-27 18:21:27 -0600

    Hopefully fix artifact uploads

--- platform/libretro/jni/Android.mk
+++ platform/libretro/jni/Android.mk
@@ -1,22 +1,20 @@
 
 LOCAL_PATH := $(call my-dir)
 
-$(info LOCAL_PATH: $(LOCAL_PATH))
+#$(info LOCAL_PATH: $(LOCAL_PATH))
 
 CORE_DIR := $(LOCAL_PATH)/../../..
 
-$(info CORE_DIR: $(CORE_DIR))
-
 include $(CORE_DIR)/platform/libretro/Makefile.common
 
-$(info SOURCES_CXX: $(SOURCES_CXX))
+#$(info SOURCES_CXX: $(SOURCES_CXX))
 
 COREFLAGS := -DANDROID -D__LIBRETRO__ -Wall -Wno-deprecated -ffunction-sections -std=c++17 -fno-rtti -fexceptions  $(INCFLAGS)
 
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE        := retro
+LOCAL_MODULE        := fake08
 LOCAL_CPP_EXTENSION := .cpp .c
 LOCAL_SRC_FILES     := $(SOURCES_CXX)
 LOCAL_C_INCLUDES    := $(MY_INCLUDES)
